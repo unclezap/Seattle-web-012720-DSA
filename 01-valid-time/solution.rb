d3f8c6b80b47209f39e4a9eab2b39cfd51d407dd
Hallie Dunn <hallie.dunn1@gmail.com>
2020-02-24 09:42:49 -0800

    lief's addition

--- 01-valid-time/solution.rb
+++ 01-valid-time/solution.rb
@@ -8,3 +8,15 @@ end
 valid_time("25:12")
 puts "second"
 valid_time("12:45")
+
+# lief adds that you can actually do greater than/less than comparisons on strings~!
+
+def valid_time2(time)
+   time = time.split(":")
+   puts "0" <= time[0] && time[0] <= "23" && "0" <= time[1] && time[1] <= "59"
+end
+
+
+valid_time2("25:12")
+puts "second"
+valid_time2("12:45")
